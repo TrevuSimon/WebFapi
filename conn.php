8ea40ea3eb400122af22e862d823eba0d61c68ba
Guilherme <guilherme1301703@fapi-pinhais.edu.br>
2018-05-10 18:03:42 -0300

    primeiro commit

---
+++ conn.php
@@ -0,0 +1,21 @@
+<<?php
+function conn()
+{
+  // code...
+}
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+} else {
+  return $conn;
+}
+?>
+ ?>
